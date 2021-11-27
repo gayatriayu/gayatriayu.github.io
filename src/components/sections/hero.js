937b35a53af6b9885f74b002c31a5b4b7c092f5e
gaayatri25 <gayatrimade30@gmail.com>
2021-11-28 06:57:09 +0700

    added files

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
 
   const one = <h1>Hello, my name is</h1>;
   const two = <h2 className="big-heading">Made Ayu Gayatri.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const three = <h3 className="big-heading">Data Analysis Enthusiast.</h3>;
   const four = (
     <>
       <p>
